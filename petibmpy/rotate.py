c800f637f1e177af022531d879c1c78dfe0f3528
mesnardo <mesnardo@gwu.edu>
2019-05-10 16:04:05 -0400

    Add 3D rotation function; rename 2D rotation function

--- petibmpy/rotate.py
+++ petibmpy/rotate.py
@@ -1,9 +1,10 @@
-"""Module with function to rotate a 2D geometry."""
+"""Module with function to rotate a geometry."""
 
+import math
 import numpy
 
 
-def rotate2d(x, y, center=(0.0, 0.0), angle=0.0, mode='deg'):
+def rotation2d(x, y, center=(0.0, 0.0), angle=0.0, mode='deg'):
     """Rotate (x, y) coordinates around a center.
 
     Parameters
@@ -36,3 +37,57 @@ def rotate2d(x, y, center=(0.0, 0.0), angle=0.0, mode='deg'):
     x_new = xc + (x - xc) * numpy.cos(angle) - (y - yc) * numpy.sin(angle)
     y_new = yc + (x - xc) * numpy.sin(angle) + (y - yc) * numpy.cos(angle)
     return x_new, y_new
+
+
+def rotation3d(x, y, z,
+               roll=0.0, yaw=0.0, pitch=0.0,
+               center=(0.0, 0.0, 0.0)):
+    """Rotate 3D point.
+
+    Parameters
+    ----------
+    x : float
+        x-coordinate of point.
+    y : float
+        y-coordinate of point.
+    z : float
+        z-coordinate of point.
+    roll : float (optional)
+        Roll angle (in radians); default: 0.0.
+    yaw : float (optional)
+        Yaw angle (in radians); default: 0.0.
+    pitch : float (optional)
+        Pitch angle (in radians); default: 0.0.
+    center : tuple of floats
+        Coordinates of the center of rotation;
+        default: [0.0, 0.0, 0.0].
+
+    Returns
+    -------
+    xr : float
+        x-coordinate of rotated point.
+    yr : float
+        y-coordinate of rotated point.
+    zr : float
+        z-coordinate of rotated point.
+
+    """
+    center = numpy.array(center)
+    Rx = numpy.array([[1.0, 0.0, 0.0],
+                      [0.0, math.cos(roll), math.sin(roll)],
+                      [0.0, -math.sin(roll), math.cos(roll)]])
+    Ry = numpy.array([[math.cos(yaw), 0.0, math.sin(yaw)],
+                      [0.0, 1.0, 0.0],
+                      [-math.sin(yaw), 0.0, math.cos(yaw)]])
+    Rz = numpy.array([[math.cos(pitch), math.sin(pitch), 0.0],
+                      [-math.sin(pitch), math.cos(pitch), 0.0],
+                      [0.0, 0.0, 1.0]])
+    point = numpy.array([x, y, z])
+    new = Rx.dot(Ry.dot(Rz.dot(point - center))) + center
+    xr, yr, zr = new
+    return xr, yr, zr
+
+
+rotation3d_vec = numpy.vectorize(rotation3d,
+                                 excluded=['roll', 'yaw', 'pitch',
+                                           'center'])
