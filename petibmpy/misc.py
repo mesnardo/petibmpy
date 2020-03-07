a819b27581c6a0e65175b5e095fc040778ffa549
mesnardo <mesnardo@gwu.edu>
2020-03-07 12:00:41 -0500

    Add function to delete datasets from HDF5 file
    and decorator to check is variable names are not primary variables.
    (In PetIBM, primary variables are "vertex", "u", "v", "w", and "p".)

--- petibmpy/misc.py
+++ petibmpy/misc.py
@@ -1,7 +1,45 @@
 """Collection of miscellaneous functions and classes."""
 
 import collections
+import functools
+import h5py
+import inspect
 import yaml
+
+
+def check_not_primary_variables(f):
+    """Check if variable names are not primary variables."""
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        forbidden_names = ('vertex', 'u', 'v', 'w', 'p')
+        func_args = inspect.getcallargs(f, *args, **kwargs)
+        names = func_args.get('names', [])
+        common_names = list(set(names) & set(forbidden_names))
+        if len(common_names) > 0:
+            raise ValueError('Attempting to delete primary variables '
+                             f'{common_names}')
+        return f(*args, **kwargs)
+    return wrapper
+
+
+@check_not_primary_variables
+def delete_datasets_hdf5(filepath, names):
+    """Delete datasets from HDF5 file.
+
+    If a name if not a dataset, the function moves to the next name.
+
+    Parameters
+    ----------
+    filepath : pathlib.Path or str
+        Path of the HDF5 file.
+    names : list or tuple
+        Names of the datasets to delete.
+
+    """
+    with h5py.File(filepath, 'a') as f:
+        for name in names:
+            if name in f.keys():
+                del f[name]
 
 
 class _Sequence(list):
